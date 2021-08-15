07b9ab056d332ace7c110623583d6a4a490e0082
Evellyn Lima <evellynlrl@hotmail.com>
2021-08-15 10:55:44 -0300

    Add cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3000
 
@@ -6,6 +7,8 @@ const controller = require('./controller')(express)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cors())
 
 app.use('/', controller)
 
