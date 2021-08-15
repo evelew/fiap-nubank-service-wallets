ebcd5df64086df8f3503f9b8bc8484c49e6a2e0f
Evellyn Lima <evellynlrl@hotmail.com>
2021-08-15 11:21:40 -0300

    Add cors options

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const controller = require('./controller')(express)
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:8100',
+  methods: 'GET, PUT, POST'
+}))
 
 app.use('/', controller)
 
