ba710aa49cc58eabaeb51d188ad9234a88feb537
Evellyn Lima <evellynlrl@hotmail.com>
2021-08-15 18:12:36 -0300

    Improve logic inside the create-new service

--- services/create-new.js
+++ services/create-new.js
@@ -1,6 +1,22 @@
+const axios = require('axios')
+
 const createNew = require('./../repository/create-new')
 
-module.exports = async (body) => {
-  const message = await createNew(body)
-  return message
+module.exports = (body) => {
+  axios
+    .get(`http://localhost:3001/income/get/${body.cdi}`)
+    .then(async (res) => {
+      const income = res.data
+
+      const data = {
+        name: body.name,
+        value: body.value,
+        cdi: income.cdi_percentage,
+        due_date: income.due_date,
+      }
+
+      const message = await createNew(data)
+      return message
+    })
+    .catch((err) => console.error(err))
 }
